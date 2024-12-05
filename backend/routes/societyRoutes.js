28e626f6f0fcd3633ac869346ec50fcf96326507
SrParth160 <parth.virani@swiftrut.com>
2024-12-06 04:40:51 +0530

    society controller

--- backend/routes/societyRoutes.js
+++ backend/routes/societyRoutes.js
@@ -1,12 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const societyController = require('../controllers/societyController');
-const authenticate = require('../middleware/auth');
+const societyController = require("../controllers/societyController");
 
-router.post('/', societyController.createSociety);
-router.get('/', societyController.getAllSocieties);
-router.get('/:id', societyController.getSocietyById);
-router.put('/:id', societyController.updateSociety);
-router.delete('/:id',  societyController.deleteSociety);
+router.post("/", societyController.createSociety);
+router.get("/", societyController.getAllSocieties);
+router.get("/:id", societyController.getSocietyById);
+router.put("/:id", societyController.updateSociety);
+router.delete("/:id", societyController.deleteSociety);
 
 module.exports = router;
